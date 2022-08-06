bb459a5a257433eb25dfcde653bd3b538207adbf
Vgnttv <o.vignani@gmail.com>
2022-08-06 14:46:49 +0200

    Allow user to enter lottery

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import web3 from './web3';
+import lottery from './lottery';
 import './App.css';
 
 class App extends Component {
+  state = { manager: '', players: [], balance: '', value: '', message: '' };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
   render() {
+    const { balance, manager, players, value, message } = this.state;
+    const balanceInEther = web3.utils.fromWei(balance, 'ether');
+
+    const onSubmit = async (e) => {
+      e.preventDefault();
+      const accounts = await web3.eth.getAccounts();
+
+      this.setState({ message: 'Waiting on transaction success' });
+
+      await lottery.methods
+        .enter()
+        .send({ from: accounts[0], value: web3.utils.toWei(value, 'ether') });
+      this.setState({ message: 'You have been entered' });
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {manager}
+          <br />
+          There are currently {players.length} people entered, competing to win{' '}
+          {balanceInEther} ether!
+        </p>
+        <hr />
+        <form onSubmit={onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => this.setState({ value: e.target.value })}
+            ></input>
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr />
+        <h1>{message}</h1>
       </div>
     );
   }
